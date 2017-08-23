8c1a07b8f7e793f180d1f49cac39e0540ee6af78
Isaac <rios.isaac@webmonster.mx>
2017-08-23 01:54:07 -0500

    jade render title

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require ('express');
 
 var app = express();
 app.set("view engine","jade");
+
+app.use(express.static("public"));
+
 app.get("/",function(solicitud,respuesta){
 	respuesta.render("index");
 });
