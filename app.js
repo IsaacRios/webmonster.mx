bf372965259a307d4c485ce7535206f430cb57c7
Isaac <rios.isaac@webmonster.mx>
2017-09-18 17:36:33 -0500

    layout

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
 var express = require ('express');
+var mongoose = require ('mongoose');
 
 var app = express();
+
+mongoose.connect("mongodb://localhost/webmonsterDB");
+var productSchema = {
+	title: String,
+	description: String,
+	imageUrl: String,
+	pricing: Number
+};
+
+var Product = mongoose.model("Product", productSchema);
+
 app.set("view engine","pug");
 
 app.use(express.static("public"));
 
 app.get("/",function(solicitud,respuesta){
+
+/*var data = {
+	title: "Producto1",
+	description: "Pieza de software",
+	imageUrl: "data.png",
+	pricing: 1000
+}
+
+var product = new Product(data);
+
+product.save(function(err){
+	console.log(product);
+});
+*/
 	respuesta.render("index");
 });
+
+
+
+app.get("/services/new",function(solicitd, respuesta){
+
+	respuesta.render("services/new")
+
+})
+
+
+
 
 app.listen(8080);
